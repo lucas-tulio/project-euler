575289e8e935d1423eca536fe5c8a3179b0df6b3
Tulio <tulio@doubleleft.com>
2015-03-02 17:30:56 -0300

    Reading args

---
+++ 7-nth-prime.py
@@ -0,0 +1,31 @@
+import time
+import sys
+
+def is_prime(num):
+
+  if num == 2: return True
+  if num % 2 == 0: return False
+
+  for i in xrange(3, num, 2):
+    if num % i == 0:
+      return False
+
+  return True
+
+def find_nth_prime(num):
+  primes = 0
+  i = 2
+  while primes < num:
+    if is_prime(i):
+      primes = primes + 1
+    i = i + 1
+  print str(num) + "th prime: " + str(i - 1)
+
+if len(sys.argv) != 2:
+  print "Example usage: python 7-nth-prime.py 10001"
+  sys.exit()
+
+start = time.time()
+find_nth_prime(int(sys.argv[1]))
+end = time.time()
+print "Time to calc: " + str(end - start)
